dc22ec5ca03cc669e48123f499f581ef8d5d29e5
Hamza-ZAIDI <fk_zaidi@esi.dz>
2019-09-10 16:23:03 +0100

    Added user login logic.

--- app/src/main/java/dz/esi/zaidi/AMEDDAH_ZAIDI/esirealestate/MainActivity.kt
+++ app/src/main/java/dz/esi/zaidi/AMEDDAH_ZAIDI/esirealestate/MainActivity.kt
@@ -1,5 +1,6 @@
 package dz.esi.zaidi.AMEDDAH_ZAIDI.esirealestate
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
@@ -148,12 +149,8 @@ class MainActivity : AppCompatActivity(),NavigationView.OnNavigationItemSelected
             }
 
             R.id.nav_bookmark -> {
-               /*TODO("Implement the logic" +
-                       "if user is logged in" +
-                       "    showFragment(BookmarksFragment()" +
-                       "else" +
-                       "    showFragment(UserLoginFragment")*/
-                showFragment(UserLoginFragment())
+                val intent = Intent(this, UserLoginFragment::class.java)
+                startActivity(intent)
             }
 
         }
--- app/src/main/java/dz/esi/zaidi/AMEDDAH_ZAIDI/esirealestate/post_details/ActionsViewModel.kt
+++ app/src/main/java/dz/esi/zaidi/AMEDDAH_ZAIDI/esirealestate/post_details/ActionsViewModel.kt
@@ -1,16 +1,25 @@
 package dz.esi.zaidi.AMEDDAH_ZAIDI.esirealestate.post_details
 
 import android.app.Application
+import android.content.Intent
+import android.provider.SyncStateContract.Helpers.update
+import android.util.Log
 import android.widget.Toast
+import androidx.core.content.ContextCompat.startActivity
 import androidx.lifecycle.AndroidViewModel
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FieldValue
+import com.google.firebase.firestore.FirebaseFirestore
+import dz.esi.zaidi.AMEDDAH_ZAIDI.esirealestate.bookmarks.UserLoginFragment
 import dz.esi.zaidi.AMEDDAH_ZAIDI.esirealestate.data.RealEstatePost
 import dz.esi.zaidi.AMEDDAH_ZAIDI.esirealestate.data.RealEstatePostsRepository
 
 class ActionsViewModel(application: Application) : AndroidViewModel(application){
-    private var realEstatePostsRepository : RealEstatePostsRepository
-    init {
-        realEstatePostsRepository = RealEstatePostsRepository(application)
-    }
+    private var realEstatePostsRepository : RealEstatePostsRepository = RealEstatePostsRepository(application)
+    private var auth : FirebaseAuth = FirebaseAuth.getInstance()
+    private var db : FirebaseFirestore = FirebaseFirestore.getInstance()
+
+
     fun addToFavorite(post : RealEstatePost){
         realEstatePostsRepository.insertRealEstatePost(post)
     }
@@ -20,11 +29,31 @@ class ActionsViewModel(application: Application) : AndroidViewModel(application)
     }
 
     fun bookmarkLink(link : String){
-        TODO( "Should implement the logic to bookmark a link")
+        if (auth.uid == null) {
+            //TODO if user not logged in, he must go to the UserLoginFragment by intent
+            Log.e("hello","user no signed in ")
+        }
+        else {
+            db
+                .collection("users")
+                .document(auth.uid!!)
+                .update("bookmarks", FieldValue.arrayUnion(link))
+        }
+
     }
 
     fun unBookmarkLink(link : String){
-        TODO( "Should implement the logic to unbookmark a link")
+        if (auth.uid == null) {
+            //TODO if user not logged in, he must go to the UserLoginFragment by intent
+            Log.e("hello","user no signed in ")
+        }
+        else {
+            db
+                .collection("users")
+                .document(auth.uid!!)
+                .update("bookmarks", FieldValue.arrayRemove(link))
+        }
+
     }
 
 
